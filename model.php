65fb95b494162d2e15adc3fe95b7277e7d315d03
y.washizawa <washizawa>
2019-05-24 13:09:37 +0900

    じゃんけん用

---
+++ model.php
@@ -0,0 +1,43 @@
+<?php
+$hands=['グー','チョキ','パー'];
+//require_once(dbaccess.php);
+
+if(isset($_REQUEST['zyanken'])){  
+ $zyanken=$_POST['zyanken'];
+}
+$key = array_rand($hands);
+$teki=$hands[$key];
+
+ switch($zyanken){
+     case ($zyanken==$teki):
+     $answer="引き分け";
+     break;
+     case 'グー':
+      if($teki=='チョキ'){
+          $answer='勝ち';
+          break;
+      }
+      else if($teki=='パー'){
+          $answer='負け';
+          break;
+      }
+      case 'チョキ':
+      if($teki=='パー'){
+          $answer='勝ち';
+          break;
+      }
+      else if($teki=='グー'){
+          $answer='負け';
+          break;
+      }
+      case 'パー':
+      if($teki=='グー'){
+          $answer='勝ち';
+          break;
+      }
+      else if($teki=='チョキ'){
+          $answer='負け';
+          break;
+      }
+ }
+>
---
+++ view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>じゃんけん</title>
+</head>
+<body>
+<form action="model.php" method="post">
+ <p>
+     <label >   
+    <img src="fig1.png">
+    <input type="radio" title="zyanken" name="zyanken" >グー
+    </label>
+    <label>
+    <img src="fig2.png">
+     <input type="radio" title="zyanken" name="zyanken" >チョキ
+     <label>
+     <img src="fig3.png">
+     <input type="radio" title="zyanken" name="zyanken" >パー
+   </label>
+     <button type="submit" class ="battole-button">
+</form>
+</body>
+</html>
